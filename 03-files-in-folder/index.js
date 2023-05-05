36c24700dde30e7a4cbbc58798a4badb1543826a
Maxim <maksim.pudeev@bk.ru>
2023-05-05 15:32:06 +0300

    feat: create 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readdir(path.resolve(__dirname, 'secret-folder'), { withFileTypes: true }, (error, files) => {
+  if (error) {
+    console.log(error);
+  } else {
+    files.forEach(file => {
+      if (file.isFile()) {
+        let fileName = file.name.split('.')[0];
+        let extension = file.name.split('.')[1];
+        fs.stat(path.resolve(__dirname, 'secret-folder', file.name), (error, stats) => {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log(`${fileName} - ${extension} - ${stats.size / 1024}kb`)
+          }
+        })
+      }
+    })
+  }
+
+})
+
