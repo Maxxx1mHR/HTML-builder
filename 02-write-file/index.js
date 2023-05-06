e0b94c3bc3322478c1483984bff31540d07c0e4e
Your Name <you@example.com>
2023-05-06 17:59:23 +0300

    feat: add check on SIGINT (ctrl + c) on close write in file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -2,10 +2,14 @@ const fs = require('fs');
 const path = require('path');
 const { stdin, stdout } = process;
 
+let writeableStream = fs.createWriteStream(path.resolve(__dirname, 'text.txt'));
 
 stdout.write('Дружище, как я рад тебя видеть! Напиши что-нибудь...\n');
 
-let writeableStream = fs.createWriteStream(path.resolve(__dirname, 'text.txt'));
+process.on('SIGINT', () => {
+  stdout.write('Ну ты это, заходи если что!\n');
+  process.exit();
+})
 
 stdin.on('data', data => {
   if (data.toString().trim() === 'exit') {
