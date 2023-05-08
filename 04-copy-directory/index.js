bb56bae134fb4e24cf413b6b7e1da45afe4a6440
Your Name <you@example.com>
2023-05-08 21:26:43 +0300

    fix: rewrite with use async/await 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,33 +2,36 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.mkdir(path.resolve(__dirname, 'files-copy'), { recursive: true }, error => {
-  if (error) throw error;
-});
+async function checkFolderExsist() {
+  let exsist;
+  await fs.promises.access(path.resolve(__dirname, 'files-copy'))
+    .then(() => {
+      exsist = true;
+    })
+    .catch(() => {
+      exsist = false;
+    });
+  return exsist;
+}
 
-
-fs.readdir(path.resolve(__dirname, 'files-copy'), (error, files) => {
-  if (error) {
-    console.log(error);
-  } else {
-    files.forEach(file => {
-      fs.unlink(path.resolve(__dirname, 'files-copy', file), error => {
-        if (error) throw error;
-      });
+async function createFolder() {
+  if (await checkFolderExsist()) {
+    await fs.promises.rm(path.resolve(__dirname, 'files-copy'), { recursive: true }, error => {
+      if (error) throw error;
     });
   }
-});
+  await fs.promises.mkdir(path.resolve(__dirname, 'files-copy'), { recursive: true }, error => {
+    if (error) throw error;
+  });
+}
 
-fs.readdir(path.resolve(__dirname, 'files'), (error, files) => {
-  if (error) {
-    console.log(error);
-  } else {
-    files.forEach(file => {
-      fs.copyFile(path.resolve(__dirname, 'files', file), path.resolve(__dirname, 'files-copy', file), error => {
-        if (error) {
-          console.log(error);
-        }
-      });
-    });
-  }
-});
+async function copyFiles() {
+  let files = '';
+  files = await fs.promises.readdir(path.resolve(__dirname, 'files'));
+  files.forEach(file => {
+    fs.promises.copyFile(path.resolve(__dirname, 'files', file), path.resolve(__dirname, 'files-copy', file));
+  });
+}
+
+createFolder()
+  .then(() => copyFiles());
